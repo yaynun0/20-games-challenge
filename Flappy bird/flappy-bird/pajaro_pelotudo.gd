f032671f116d41e5dd8b0e86b8ea5b6b6b4813f3
yaynun0 <l.gerardonun@gmail.com>
2025-09-14 23:04:59 -0600

    rotacion limitada

--- Flappy bird/flappy-bird/pajaro_pelotudo.gd
+++ Flappy bird/flappy-bird/pajaro_pelotudo.gd
@@ -10,11 +10,21 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready() -> void:
 	position.x = 150
 
+func rotate_vel(vel):
+	if 90>rotation_degrees and rotation_degrees >-90:
+		rotation_degrees+=atan(vel)
+	if rotation_degrees >90 and vel<0:
+		rotation_degrees=89
+	elif rotation_degrees <-90 and vel>0:
+		rotation_degrees=-89
+
+		
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
-	
+	rotate_vel(velocity.y)
 	if Input.is_action_just_pressed("FLAP") :
 		velocity.y = jump_speed
 		animated_sprite_2d.play("FLAP" )
