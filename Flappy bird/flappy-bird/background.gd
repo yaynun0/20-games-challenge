fd7d8298d146936d303587b676d21286399298a4
ilovebees <65438052+ilovebees@users.noreply.github.com>
2025-09-14 21:19:14 -0600

    ACTUALIZAR BACKGROUND

--- Flappy bird/flappy-bird/background.gd
+++ Flappy bird/flappy-bird/background.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var texture_rect_2: TextureRect = $TextureRect2
 
 var speed=300
+var direction = Vector2(-1.0,0.0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	texture_rect.position.x=0
@@ -16,16 +17,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if texture_rect.position.x>=-576:
-		texture_rect.position += Vector2(-1,0)*speed*delta
-	else:
-		texture_rect.position.x=576
-		texture_rect.position += Vector2(-1,0)*speed*delta
+	texture_rect.position += direction*speed*delta
 	
-	if texture_rect_2.position.x>=-576:
-		texture_rect_2.position += Vector2(-1,0)*speed*delta
-	else:
-		texture_rect_2.position.x=576
-		texture_rect_2.position += Vector2(-1,0)*speed*delta
-	
+	if texture_rect.position.x <= -576:
+		texture_rect.position.x = 0
+
+	texture_rect_2.position += direction *speed *delta
+	if texture_rect_2.position.x <= 0:
+		texture_rect_2.position.x = 576
 	
